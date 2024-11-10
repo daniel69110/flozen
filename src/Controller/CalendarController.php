6d26cf5b075b26a5a6cee444d8b4deb4aea2ece3
daniel <danielcourot@hotmail.com>
2024-11-10 19:43:47 +0100

    change responisve calendar

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -11,7 +11,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+// #[IsGranted('ROLE_USER')]
 class CalendarController extends AbstractController
 {
     #[Route('/calendar', name: 'calendar')]
